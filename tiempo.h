03bcae430b0726f3c821d65033b6edd3018ca01b
NicolasPinillaArdila <pinillaa.nicolas@javeriana.edu.co>
2025-11-15 00:09:11 -0500

    Add files via upload

---
+++ tiempo.h
@@ -0,0 +1,12 @@
+/*#######################################################################################
+#* Archivo: tiempo.h
+#* Descripción: Declaraciones para medir tiempos de ejecución con gettimeofday.
+######################################################################################*/
+
+#ifndef TIEMPO_H
+#define TIEMPO_H
+
+void InicioMuestra();
+void FinMuestra();
+
+#endif
